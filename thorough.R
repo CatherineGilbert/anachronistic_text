73a90c28d95f8df36023b2a312c7fab19f778c33
Catherine Gilbert <cmg3@illinois.edu>
2023-03-09 17:06:49 -0600

    fix word error

--- thorough.R
+++ thorough.R
@@ -30,18 +30,41 @@ text <- text[!str_detect(text, "’")]
 #turn the remaining uncommon words into chunks to be ngram'd
 text.chunks <- chunk(text, len = 12)
 
+ngram(text.chunks[[i]][6], year_start = target.year-1)
+ngram(text.chunks[[i]][6], year_start = target.year-1, year_end = target.year)
+
 #get the data for uncommon words
 stats <- tibble()
 for (i in 1:length(text.chunks)) {
-  continue <- F
-  while (continue == F) {
-    continue <- T
-    tryCatch({
-      old <- ngram(text.chunks[[i]], year_start = target.year-1, year_end = target.year) %>% as_tibble()
-      new <- ngram(text.chunks[[i]], year_start = 2018, year_end = 2019) %>% as_tibble()
-    },
-    error = function(e){continue <- F})
-  }
+  tryCatch({
+    old <- ngram(text.chunks[[i]], year_start = target.year-1, year_end = target.year) %>% as_tibble()
+    new <- ngram(text.chunks[[i]], year_start = 2018, year_end = 2019) %>% as_tibble()
+    stats <- rbind(stats, new, old)
+    }, error = function(e){
+      for (n in 1:length(text.chunks[[i]])) {
+        tryCatch({
+          old <- ngram(text.chunks[[i]][n], year_start = target.year-1, year_end = target.year) %>% as_tibble()
+          new <- ngram(text.chunks[[i]][n], year_start = 2018, year_end = 2019) %>% as_tibble()
+          print(paste("chunk",i,"word",n))
+          stats <- rbind(stats, new, old)
+        }, error = function(e){
+          print(paste(n,"again"))
+          all <- ngram(text.chunks[[i]][n], year_start = target.year) %>% as_tibble()
+          stats <- rbind(stats, all)
+        })
+      }
+    })
+  print(i/length(text.chunks))
+}
+stats <- filter(stats, Year == target.year | Year == max(Year)) %>% distinct() 
+
+
+
+#get the data for uncommon words
+stats <- tibble()
+for (i in 1:length(text.chunks)) {
+  old <- ngram(text.chunks[[i]], year_start = target.year-1, year_end = target.year) %>% as_tibble()
+  new <- ngram(text.chunks[[i]], year_start = 2018, year_end = 2019) %>% as_tibble()
   stats <- rbind(stats, new, old)
   print(i/length(text.chunks))
 }
